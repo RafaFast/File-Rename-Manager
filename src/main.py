a395f1068a7483bc8c5220678fdbe0b6883b8da6
Rafael S. Novo <rafaelnovo.st@gmail.com>
2023-12-29 15:55:06 -0300

    fix existing file error

--- src/main.py
+++ src/main.py
@@ -1,29 +1,25 @@
 import os
 
 class File:
-    def __init__(self, dir, old, new):
+    def __init__(self, dir, sentence, replaced):
         self.dir = dir
-        self.old = old
-        self.new = new
+        self.sentence = sentence
+        self.replaced = replaced
         self.files = []
         self.set_files_path()
-        self.set_new_name()
-        self.reset_files()
+        self.renamed_files = [file.replace(self.sentence, self.replaced) for file in self.files]
+        self.rename_files()
 
     def set_files_path(self):
         for file in os.listdir(self.dir):
-            self.files.append(self.dir + f'\{file}')
-
-    def set_new_name(self):
-        for file in self.files:
-            if self.old in file:
-                renamed_file = file.replace(self.old, self.new)
-                yield renamed_file
-
-    def reset_files(self):
-        self.files.clear()
-        self.newfiles = self.set_new_name()
+            self.files.append(os.path.join(self.dir, file))
 
     def rename_files(self):
-        for file in self.files:
-            os.rename(file, self.newfiles)
+        for old, new in zip(self.files, self.renamed_files):
+            os.rename(old, new)
+
+def main():
+    pasta = File('C:\Users\Rafael Novo\Desktop\Curso de SQL', '(360P)', '')
+    print(os.listdir(pasta.dir))
+
+main()
